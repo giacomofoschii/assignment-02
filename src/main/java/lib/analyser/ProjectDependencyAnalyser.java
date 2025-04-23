a49902244459c49947cbfa9d34119ccfb15d2b06
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-04-23 10:21:58 +0200

    [ASYNC] Minor changes

--- src/main/java/lib/analyser/ProjectDependencyAnalyser.java
+++ src/main/java/lib/analyser/ProjectDependencyAnalyser.java
@@ -3,6 +3,7 @@ package lib.analyser;
 /**
  * Asynchronously analyzes dependencies in a full Java project.
  * Scans all packages and classes to collect used types.
+ * Aggregates and return results for the entire project.
  */
 public class ProjectDependencyAnalyser {
 }
