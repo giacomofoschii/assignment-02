29578db020b6a5888444083c21251bb988f4d30f
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-04-30 13:01:21 +0200

    [REACTIVE] Added first implementation of GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -9,15 +11,31 @@ repositories {
     mavenCentral()
 }
 
+javafx {
+    version = "24"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.graphics")
+}
+
+application {
+    mainClass.set("gui.codeAnalyzer.DependencyAnalyser")
+    applicationDefaultJvmArgs = listOf(
+        "--add-modules=javafx.controls,javafx.fxml,javafx.graphics",
+        "--add-exports=javafx.graphics/com.sun.javafx.util=ALL-UNNAMED",
+        "--add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED",
+        "--add-exports=javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED"
+    )
+}
+
 dependencies {
     implementation("com.github.javaparser:javaparser-core:3.25.4")
     implementation("com.github.javaparser:javaparser-symbol-solver-core:3.25.4")
     implementation("io.vertx:vertx-core:4.4.2")
-    implementation("io.vertx:vertx-junit5:4.4.2")
 
-    implementation("org.openjfx:javafx-base:21.0.2")
-    implementation("org.openjfx:javafx-controls:21.0.2")
-    implementation("org.openjfx:javafx-graphics:21.0.2")
+    implementation("org.graphstream:gs-core:2.0")
+    implementation("org.graphstream:gs-ui-javafx:2.0")
+    implementation("org.graphstream:gs-algo:2.0")
+
+    implementation("io.reactivex.rxjava3:rxjava:3.1.6")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
