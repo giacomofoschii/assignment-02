176172dcfedb496f8c9fe82e0a77c6bf74ac9360
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-05-05 16:07:50 +0200

    [REACTIVE] Added model structure

---
+++ src/main/java/reactive/model/ReactiveDependencyAnalyser.java
@@ -0,0 +1,10 @@
+package reactive.model;
+
+/**
+ * This class is responsible for analyzing the dependencies of a Java project.
+ * It uses JavaParser to extract dependencies from the source files and models
+ * them using the PackageDependency, ClassDependency, and ProjectDependency classes.
+ */
+public class ReactiveDependencyAnalyser {
+
+}
